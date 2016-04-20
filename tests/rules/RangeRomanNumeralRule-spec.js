409bb5cabe877e37a0d62fa7422f37130a159a5d
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-20 09:59:01 -0400

    Added sanity check tests for range rule

--- tests/rules/RangeRomanNumeralRule-spec.js
+++ tests/rules/RangeRomanNumeralRule-spec.js
@@ -17,6 +17,18 @@ describe("RangeRomanNumeralRule", function () {
 
         it("should return false if number is after range end", function () {
             rule.applies(60).should.equal(false);
-        })
+        });
+
+        it("should return false if number is equal to range end", function () {
+            rule.applies(50).should.equal(false);
+        });
+
+        it("should return true if number is equal to range start", function () {
+            rule.applies(40).should.equal(true);
+        });
+
+        it("should return true if number is inside range", function () {
+            rule.applies(45).should.equal(true);
+        });
     });
 });
