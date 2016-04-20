bfa4496fc78054ba1ce856955f718b17a11529f3
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-20 09:57:10 -0400

    applies returns false if number is after range end

--- src/rules/RangeRomanNumeralRule.js
+++ src/rules/RangeRomanNumeralRule.js
@@ -3,14 +3,15 @@
 let BaseRomanNumeralRule = require("./BaseRomanNumeralRule");
 
 class RangeRomanNumeralRule extends BaseRomanNumeralRule {
-    constructor(arabicNumber, numeral, rangeStart) {
+    constructor(arabicNumber, numeral, rangeStart, rangeEnd) {
         super(arabicNumber, numeral);
 
         this._rangeStart = rangeStart;
+        this._rangeEnd = rangeEnd;
     }
 
     applies(number) {
-        return number >= this._rangeStart;
+        return number >= this._rangeStart && number < this._rangeEnd;
     }
 }
 
