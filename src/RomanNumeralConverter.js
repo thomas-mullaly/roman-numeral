f684446523bd5284d12d7d4c0ad3cbaf9f7a5209
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-19 21:01:15 -0400

    Implemented support for converting 5 to V

--- src/RomanNumeralConverter.js
+++ src/RomanNumeralConverter.js
@@ -6,7 +6,10 @@ class RomanNumeralConverter {
         let result = "";
 
         while (i > 0) {
-            if (i === 4) {
+            if (i === 5) {
+                result += "V";
+                i -= 5;
+            } else if (i === 4) {
                 result += "IV";
                 i -= 4;
             } else {
