07b94d1b7f5743d03c6b121dbd8b177bf2a125de
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-20 09:54:19 -0400

    Added test that applies returns false if number is after range end

--- tests/rules/RangeRomanNumeralRule-spec.js
+++ tests/rules/RangeRomanNumeralRule-spec.js
@@ -8,11 +8,15 @@ describe("RangeRomanNumeralRule", function () {
         let rule = null;
 
         beforeEach(function () {
-            rule = new RangeRomanNumeralRule(40, "XL", 40);
+            rule = new RangeRomanNumeralRule(40, "XL", 40, 50);
         });
 
         it("should return false if number is before range start", function () {
             rule.applies(39).should.equal(false);
         });
+
+        it("should return false if number is after range end", function () {
+            rule.applies(60).should.equal(false);
+        })
     });
 });
